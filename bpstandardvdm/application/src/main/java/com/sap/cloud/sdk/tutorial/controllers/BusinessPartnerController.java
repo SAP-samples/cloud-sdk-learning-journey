d3d405bcf4501a25f7a4b41efb64cee8044671c8
Milton Chandradas <milton.chandradas@sap.com>
2023-02-03 16:37:31 +0000

    resilience...

--- bpstandardvdm/application/src/main/java/com/sap/cloud/sdk/tutorial/controllers/BusinessPartnerController.java
+++ bpstandardvdm/application/src/main/java/com/sap/cloud/sdk/tutorial/controllers/BusinessPartnerController.java
@@ -11,22 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultDestination;
 import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
-import com.sap.cloud.sdk.datamodel.odata.helper.Order;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
+import com.sap.cloud.sdk.tutorial.utils.GetBusinessPartnersCommand;
 
 @RestController
 public class BusinessPartnerController {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(BusinessPartnerController.class);
 
-    private static final String CATEGORY_PERSON = "1";
-    private static final String APIKEY_HEADER = "apikey";
-    private static final String SANDBOX_APIKEY = "<YOUR API GOES HERE>";
-
-    @RequestMapping( value = "/getBusinessPartners", method = RequestMethod.GET )
+    @RequestMapping(value = "/getBusinessPartners", method = RequestMethod.GET)
     public String getBusinessPartners() {
-
         final HttpDestination destination = DefaultDestination.builder()
                                                 .property("Name", "mydestination")
                                                 .property("URL", "http://localhost:8081")
@@ -34,6 +28,7 @@ public class BusinessPartnerController {
                                                 .property("Authentication", "NoAuthentication")
                                                 .build().asHttp();
 
+        // TODO: uncomment the line below, if you are using the sandbox service
         // final HttpDestination destination = DefaultDestination.builder()
         //                                         .property("Name", "mydestination")
         //                                         .property("URL", "https://sandbox.api.sap.com/s4hanacloud")
@@ -41,22 +36,9 @@ public class BusinessPartnerController {
         //                                         .property("Authentication", "NoAuthentication")
         //                                         .build().asHttp();
 
-        final List<BusinessPartner> businessPartners =
-                    new DefaultBusinessPartnerService()
-                            .getAllBusinessPartner()
-                            .select(BusinessPartner.BUSINESS_PARTNER,
-                                    BusinessPartner.LAST_NAME,
-                                    BusinessPartner.FIRST_NAME,
-                                    BusinessPartner.IS_MALE,
-                                    BusinessPartner.IS_FEMALE,
-                                    BusinessPartner.CREATION_DATE)
-                            .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
-                            .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
-                            .top(200)
-                            .withHeader(APIKEY_HEADER, SANDBOX_APIKEY)
-                            .executeRequest(destination);
+        final List<BusinessPartner> businessPartners = new GetBusinessPartnersCommand(destination).execute();
 
-            logger.info(String.format("Found %d business partner(s).", businessPartners.size()));
+        logger.info(String.format("Found %d business partner(s).", businessPartners.size()));
 
         return new Gson().toJson(businessPartners);
     }
---
+++ bpstandardvdm/application/src/main/java/com/sap/cloud/sdk/tutorial/utils/GetBusinessPartnersCommand.java
@@ -0,0 +1,79 @@
+package com.sap.cloud.sdk.tutorial.utils;
+
+import java.util.List;
+import java.time.Duration;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceConfiguration;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceDecorator;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceIsolationMode;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceRuntimeException;
+import com.sap.cloud.sdk.datamodel.odata.client.exception.ODataException;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
+import com.sap.cloud.sdk.datamodel.odata.helper.Order;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
+
+public class GetBusinessPartnersCommand {
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(GetBusinessPartnersCommand.class);
+
+    private static final String CATEGORY_PERSON = "1";
+    private final HttpDestination destination;
+    private static final String APIKEY_HEADER = "apikey";
+    private static final String SANDBOX_APIKEY = "YOUR APIKEY GOES HERE";
+
+    private final BusinessPartnerService  businessPartnerService;
+    private final ResilienceConfiguration myResilienceConfig;
+
+    public GetBusinessPartnersCommand(HttpDestination destination) {
+        this(destination, new DefaultBusinessPartnerService());
+    }
+
+    public GetBusinessPartnersCommand(HttpDestination destination, BusinessPartnerService service) {
+        this.destination = destination;
+        businessPartnerService = service;
+
+        myResilienceConfig = ResilienceConfiguration.of(BusinessPartnerService.class)
+                .isolationMode(ResilienceIsolationMode.TENANT_AND_USER_OPTIONAL)
+                .timeLimiterConfiguration(
+                        ResilienceConfiguration.TimeLimiterConfiguration.of()
+                                .timeoutDuration(Duration.ofMillis(10000)))
+                .bulkheadConfiguration(
+                        ResilienceConfiguration.BulkheadConfiguration.of()
+                                .maxConcurrentCalls(20));
+    }
+
+    public List<BusinessPartner> execute() {
+        return ResilienceDecorator.executeSupplier(this::run, myResilienceConfig, e -> {
+            logger.warn("Fallback called because of exception.", e);
+            return Collections.emptyList();
+        });
+    }
+
+    private List<BusinessPartner> run() {
+        try {
+            return businessPartnerService
+                    .getAllBusinessPartner()
+                    .select(BusinessPartner.BUSINESS_PARTNER,
+                            BusinessPartner.LAST_NAME,
+                            BusinessPartner.FIRST_NAME,
+                            BusinessPartner.IS_MALE,
+                            BusinessPartner.IS_FEMALE,
+                            BusinessPartner.CREATION_DATE
+                    )
+                    .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
+                    .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
+                    .top(200)
+                    // TODO: Uncomment the line below, if you are using the sandbox service
+                    // .withHeader(APIKEY_HEADER, SANDBOX_APIKEY)
+                    .executeRequest(destination);
+        } catch (ODataException e) {
+            throw new ResilienceRuntimeException(e);
+        }
+    }
+}
