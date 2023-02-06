1668c7f1f95af664beca58c6fe8b642524f4da3d
Milton Chandradas <milton.chandradas@sap.com>
2023-02-06 18:39:13 +0000

    crud operations...

--- cloudsdk_bp_ts/src/business-partner/business-partner.controller.ts
+++ cloudsdk_bp_ts/src/business-partner/business-partner.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, HttpException } from '@nestjs/common';
-import { BusinessPartner } from '../../services/business-partner-service';
+import { Body, Controller, Delete, Get, HttpCode, HttpException, Param, Post, Put } from '@nestjs/common';
+import { BusinessPartner, BusinessPartnerAddress } from '../../services/business-partner-service';
 import { BusinessPartnerService } from './business-partner.service';
 
 @Controller('business-partner')
@@ -16,5 +16,40 @@ export class BusinessPartnerController {
                     500
                 );
             });
+    }
+
+    @Get('/:id')
+    async getBusinessPartnerById(@Param('id') id: string): Promise<BusinessPartner> {
+        return await this.businessPartnerService.getBusinessPartnerById(id);
+    }
+
+    @Post("/:businessPartnerId/address")
+    @HttpCode(201)
+    async createAddress(
+        @Body() requestBody: Record<string, any>,
+        @Param("businessPartnerId") businessPartnerId: string): Promise<BusinessPartnerAddress> {
+
+        return await this.businessPartnerService.createAddress(requestBody, businessPartnerId);
+    }
+
+    @Put('/:businessPartnerId/address/:addressId')
+    async updateBusinessPartnerAddress(
+        @Body() requestBody: Record<string, any>,
+        @Param('businessPartnerId') businessPartnerId: string,
+        @Param('addressId') addressId: string): Promise<BusinessPartnerAddress> {
+
+        return await this.businessPartnerService.updateAddress(requestBody, businessPartnerId, addressId);
+    }
+
+    @Delete("/:businessPartnerId/address/:addressId")
+    @HttpCode(204)
+    async deleteBusinessPartnerAddress(
+        @Param("businessPartnerId") businessPartnerId: string,
+        @Param("addressId") addressId: string): Promise<void> {
+
+        return await this.businessPartnerService.deleteAddress(
+            businessPartnerId,
+            addressId
+        );
     }
 }
--- cloudsdk_bp_ts/src/business-partner/business-partner.service.ts
+++ cloudsdk_bp_ts/src/business-partner/business-partner.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import {
     businessPartnerService,
-    BusinessPartner
+    BusinessPartner,
+    BusinessPartnerAddress
 } from '../../services/business-partner-service';
 import { resilience } from '@sap-cloud-sdk/resilience';
+
+const { businessPartnerApi, businessPartnerAddressApi } = businessPartnerService();
 
 @Injectable()
 export class BusinessPartnerService {
     async getAllBusinessPartners(): Promise<BusinessPartner[]> {
 
-        const { businessPartnerApi } = businessPartnerService();
         return await businessPartnerApi
                         .requestBuilder()
                         .getAll()
@@ -19,10 +21,85 @@ export class BusinessPartnerService {
                             businessPartnerApi.schema.FIRST_NAME,
                             businessPartnerApi.schema.LAST_NAME
                         )
+                        .filter(businessPartnerApi.schema.BUSINESS_PARTNER_CATEGORY.equals("1"))
                         .top(10)
                         .addCustomHeaders({apiKey: process.env.APIKEY})
                         .execute({
                             url: process.env.URL
                         });
+    }
+
+    async getBusinessPartnerById(id: string): Promise<BusinessPartner> {
+        return await businessPartnerApi
+            .requestBuilder()
+            .getByKey(id)
+            .select(
+                businessPartnerApi.schema.BUSINESS_PARTNER,
+                businessPartnerApi.schema.FIRST_NAME,
+                businessPartnerApi.schema.LAST_NAME,
+                businessPartnerApi.schema.TO_BUSINESS_PARTNER_ADDRESS.select(
+                    businessPartnerAddressApi.schema.BUSINESS_PARTNER,
+                    businessPartnerAddressApi.schema.ADDRESS_ID,
+                    businessPartnerAddressApi.schema.POSTAL_CODE,
+                    businessPartnerAddressApi.schema.CITY_NAME,
+                    businessPartnerAddressApi.schema.STREET_NAME,
+                    businessPartnerAddressApi.schema.HOUSE_NUMBER
+                )
+            )
+            .middleware(resilience({ timeout: 10000, circuitBreaker: true, retry: true }))
+            .addCustomHeaders({ apikey: process.env.APIKEY })
+            .execute({
+                url: process.env.URL
+            });
+    }
+
+    async createAddress(
+        address: Record<string, any>,
+        businessPartnerId: string): Promise<BusinessPartnerAddress> {
+
+        const businessPartnerAddress = businessPartnerAddressApi
+            .entityBuilder()
+            .fromJson({ businessPartner: businessPartnerId, ...address });
+
+        return await businessPartnerAddressApi
+            .requestBuilder()
+            .create(businessPartnerAddress)
+            .middleware(resilience({ timeout: 10000, circuitBreaker: true, retry: true }))
+            .addCustomHeaders({ apikey: process.env.APIKEY })
+            .execute({
+                url: process.env.URL
+            });
+    }
+
+    async updateAddress(
+        address: Record<string, any>,
+        businessPartnerId: string, addressId: string): Promise<BusinessPartnerAddress> {
+
+        const businessPartnerAddress = businessPartnerAddressApi
+            .entityBuilder()
+            .fromJson({ businessPartner: businessPartnerId, addressId, ...address });
+
+        return await businessPartnerAddressApi
+            .requestBuilder()
+            .update(businessPartnerAddress)
+            .middleware(resilience({ timeout: 10000, circuitBreaker: true, retry: true }))
+            .addCustomHeaders({ apikey: process.env.APIKEY })
+            .execute({
+                url: process.env.URL
+            });
+    }
+
+    async deleteAddress(
+        businessPartnerId: string,
+        addressId: string): Promise<void> {
+
+        return await businessPartnerAddressApi
+            .requestBuilder()
+            .delete(businessPartnerId, addressId)
+            .middleware(resilience({ timeout: 10000, circuitBreaker: true, retry: true }))
+            .addCustomHeaders({ apikey: process.env.APIKEY })
+            .execute({
+                url: process.env.URL
+            });
     }
 }
