d5f7af02fa87b6f216c49c9b09064059c7084ad3
Milton Chandradas <milton.chandradas@sap.com>
2023-02-06 18:20:38 +0000

    resilience typescript...

--- cloudsdk_bp_ts/src/business-partner/business-partner.service.ts
+++ cloudsdk_bp_ts/src/business-partner/business-partner.service.ts
@@ -3,6 +3,7 @@ import {
     businessPartnerService,
     BusinessPartner
 } from '../../services/business-partner-service';
+import { resilience } from '@sap-cloud-sdk/resilience';
 
 @Injectable()
 export class BusinessPartnerService {
@@ -12,6 +13,12 @@ export class BusinessPartnerService {
         return await businessPartnerApi
                         .requestBuilder()
                         .getAll()
+                        .middleware(resilience({ timeout: 10000, circuitBreaker: true, retry: true }))
+                        .select(
+                            businessPartnerApi.schema.BUSINESS_PARTNER,
+                            businessPartnerApi.schema.FIRST_NAME,
+                            businessPartnerApi.schema.LAST_NAME
+                        )
                         .top(10)
                         .addCustomHeaders({apiKey: process.env.APIKEY})
                         .execute({
