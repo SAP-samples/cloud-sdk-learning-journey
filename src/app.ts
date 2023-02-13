17133e55fd03ea22970b7bfccac017b43d1d40fa
I335355 <madalina.vieriu@sap.com>
2023-02-13 10:59:32 +0200

    create request  to business partner API

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,30 @@
-const helloMessage: string = "Hello World!";
-console.log(helloMessage);
+import {
+  BusinessPartner,
+  businessPartnerService,
+} from "@sap/cloud-sdk-vdm-business-partner-service";
+
+async function getAllBusinessPartners(): Promise<BusinessPartner[]> {
+  const { businessPartnerApi } = businessPartnerService();
+  return await businessPartnerApi
+    .requestBuilder()
+    .getAll()
+    .top(1)
+    .addCustomHeaders({
+      APIKey: "",
+    })
+    .execute({
+      url: "https://sandbox.api.sap.com/s4hanacloud",
+    });
+}
+
+// execute request
+getAllBusinessPartners().then(
+  // success
+  (result: BusinessPartner[]) => {
+    console.log(JSON.stringify(result, null, "\t"));
+  },
+  // error
+  (error: any) => {
+    console.log("Error: " + error.message);
+  }
+);
