c82f3aca083cf9ebbaabecec6a0a62b786f49476
Paolo Ricciarelli (private) <paolo_ricciarelli@hotmail.com>
2023-02-13 16:44:18 +0000

    Included destination to S4C tenant

--- bpstandardvdm/application/src/main/java/com/sap/cloud/sdk/tutorial/controllers/BusinessPartnerController.java
+++ bpstandardvdm/application/src/main/java/com/sap/cloud/sdk/tutorial/controllers/BusinessPartnerController.java
@@ -21,6 +21,8 @@ public class BusinessPartnerController {
 
     @RequestMapping(value = "/getBusinessPartners", method = RequestMethod.GET)
     public String getBusinessPartners() {
+        
+        // Destination to the local Mock Server
         final HttpDestination destination = DefaultDestination.builder()
                                                 .property("Name", "mydestination")
                                                 .property("URL", "http://localhost:8081")
@@ -28,12 +30,22 @@ public class BusinessPartnerController {
                                                 .property("Authentication", "NoAuthentication")
                                                 .build().asHttp();
 
-        // TODO: uncomment the line below, if you are using the sandbox service
+        // Destination to the SAP API Business Hub Sandbox
         // final HttpDestination destination = DefaultDestination.builder()
         //                                         .property("Name", "mydestination")
         //                                         .property("URL", "https://sandbox.api.sap.com/s4hanacloud")
         //                                         .property("Type", "HTTP")
         //                                         .property("Authentication", "NoAuthentication")
+        //                                         .build().asHttp();
+
+        // Destination to a SAP S/4HANA Cloud (public edition) tenant
+        //final HttpDestination destination = DefaultDestination.builder()
+        //                                         .property("Name", "mydestination")
+        //                                         .property("URL", "https://my######.s4hana.ondemand.com")
+        //                                         .property("Type", "HTTP")
+        //                                         .property("Authentication", "BasicAuthentication")
+        //                                         .property("User", "ADDRESS_MANAGER_###")
+        //                                         .property("Password", "WelcomeToTheClouds1!")
         //                                         .build().asHttp();
 
         final List<BusinessPartner> businessPartners = new GetBusinessPartnersCommand(destination).execute();
