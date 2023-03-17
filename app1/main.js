1b3693846c5ab08479b2f9418fa3bd8ae1120ca9
I335355 <madalina.vieriu@sap.com>
2023-03-17 18:35:30 +0200

    refactor anchor link for node project

--- app1/main.js
+++ app1/main.js
@@ -10,9 +10,7 @@ app.get("/", function (req, res) {
     "<h1>" +
     title +
     "</h1>" +
-    '<p>Navigate to <a href="' +
-    a2url +
-    '">Application 2</a></p>' +
+    `<p>Navigate to  <a href="${a2url}"> Application 2</p>` +
     "</body></html>";
 
   res.send(page);
