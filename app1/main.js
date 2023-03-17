c32189728b3328fc9b20909f798c14d7eaf5bad3
I335355 <madalina.vieriu@sap.com>
2023-03-17 18:09:23 +0200

    add env variables in launch.json

--- app1/main.js
+++ app1/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.get("/", function (req, res) {
-  const title = process.env.TITLE1;
+  const title = process.env.TITLE;
   const a2url = process.env.A2URL;
 
   const page =
