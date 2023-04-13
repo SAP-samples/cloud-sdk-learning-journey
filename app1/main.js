b9feebb42a77a2b360d01e7ecb7a331d6240b2e6
Paolo Ricciarelli (private) <paolo_ricciarelli@hotmail.com>
2023-04-13 09:14:37 +0000

    ...

--- app1/main.js
+++ app1/main.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
   var page =
-    "<!DOCTYPE html><html><body>" + "<h1>Hello Node!</h1>" + "</body></html>";
+    "<!DOCTYPE html><html><body>" + 
+    "<h1>Hello Node!</h1>" + 
+    "</body></html>";
 
   res.send(page);
 });
