4e853c3c0384732bb05441ce249b6e6aee9eb548
Madalina Vieriu <madalina.vieriu@sap.com>
2023-05-26 12:00:03 +0300

    add services in app module

--- ts-adman/src/app.module.ts
+++ ts-adman/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BusinessPartnerController } from './adman/business-partner.controller';
+import { BusinessPartnerService } from './adman/business-partner.service';
+import { BusinessPartnerAddressController } from './adman/business-partner-address.controller';
+import { BusinessPartnerAddressService } from './adman/business-partner-address.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    BusinessPartnerController,
+    BusinessPartnerAddressController,
+  ],
+  providers: [
+    AppService,
+    BusinessPartnerService,
+    BusinessPartnerAddressService,
+  ],
 })
 export class AppModule {}
